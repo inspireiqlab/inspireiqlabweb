6e497c973251c991aae8640af85fafc91cf9cc6c
= <subinayp108@gmail.com>
2025-08-17 19:58:42 +0530

    notice updated

--- app/components/StickyNotice/index.tsx
+++ app/components/StickyNotice/index.tsx
@@ -2,6 +2,12 @@
 import { useState } from 'react';
 import Link from 'next/link';
 
+const notice = {
+  label: "🎓 Join our Free Online Session on Artificial Intelligence and Our Future",
+  date: "25th August, 11:00 AM",
+  link: "https://forms.gle/VXQ66rw9E83rLDA96"
+};
+
 export default function StickyNotice() {
   const [show, setShow] = useState(true);
 
@@ -11,10 +17,10 @@ export default function StickyNotice() {
     <div className="sticky bottom-0 z-50 bg-black text-white px-4 py-3 flex items-center justify-between shadow-md">
       <div className="flex flex-col flex-grow justify-center sm:flex-row sm:items-center gap-2">
         <p className="font-medium text-justify">
-          🎓 Join our Free Career Guide Session on Cyber Security and Ethical hacking for school students – <span className="underline">3rd August, 03:00 PM</span>
+          {notice.label} – <span className="underline">{notice.date}</span>
         </p>
         <Link
-          href="https://docs.google.com/forms/d/e/1FAIpQLSd3qw4YuDCj46fxdaJRx6cEE4CuV0geeq_7IPvMDgep_mBXQg/viewform"
+          href={notice.link}
           target="_blank"
           className="text-sm bg-[#173fae] text-center text-blue-600 font-semibold px-3 py-1 rounded hover:bg-gray-100 transition"
         >
